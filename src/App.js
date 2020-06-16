a91a0ca5d94f72f3327b57f31f875e13edea03cf
Tanay12 <trohatgi@connect.ust.hk>
2020-06-16 21:53:44 +0530

    feat: state practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, {Component} from "react";
+import React, {Component} from "react"
 
-// Challenge:
-// Given an incomplete class-based component without a constructor,
-// add a constructor and initialize state to fix the broken component.
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            name: "Tanay",
-            age: 22
+            isLoggedIn: false,
         }
     }
     render() {
         return (
             <div>
-                <h1>{this.state.name}</h1>
-                <h3>{this.state.age} years old</h3>
+                <h1>You are currently logged {this.state.isLoggedIn? "in": "out"}</h1>
             </div>
         )
     }
-
 }
 
 export default App
